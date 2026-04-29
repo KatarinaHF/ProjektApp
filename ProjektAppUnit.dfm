a7ad036dd586deeb4cd3f4777dde630d578bf7f1
CarolinePra <caroline.p.pradel@gmail.com>
2026-04-29 09:16:40 +0200

    Calender prototype created

--- ProjektAppUnit.dfm
+++ ProjektAppUnit.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 760
-  ClientWidth = 988
+  ClientHeight = 500
+  ClientWidth = 700
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,19 +12,19 @@ object Form1: TForm1
   Font.Style = []
   TextHeight = 15
   object LogInButton: TButton
-    Left = 8
+    Left = 4
     Top = 8
-    Width = 426
-    Height = 744
+    Width = 350
+    Height = 500
     Caption = 'Log In'
     TabOrder = 0
     OnClick = LogInClick
   end
   object Memo1: TMemo
-    Left = 440
+    Left = 342
     Top = 8
-    Width = 540
-    Height = 744
+    Width = 350
+    Height = 500
     Lines.Strings = (
       'Memo1')
     TabOrder = 1
---
+++ Unit2.pas
@@ -0,0 +1,74 @@
+unit Unit2;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    RadioGroup1: TRadioGroup;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    Panel1: TPanel;
+    Monday: TMemo;
+    Tuesday: TMemo;
+    Wednesday: TMemo;
+    Memo5: TMemo;
+    Memo6: TMemo;
+    Memo7: TMemo;
+    Memo8: TMemo;
+    Memo9: TMemo;
+    Memo10: TMemo;
+    Memo11: TMemo;
+    Memo12: TMemo;
+    Memo13: TMemo;
+    Memo14: TMemo;
+    Memo15: TMemo;
+    Memo16: TMemo;
+    Memo17: TMemo;
+    Memo18: TMemo;
+    Memo19: TMemo;
+    Memo20: TMemo;
+    Memo21: TMemo;
+    Memo22: TMemo;
+    Memo23: TMemo;
+    Memo24: TMemo;
+    Memo25: TMemo;
+    Memo26: TMemo;
+    Memo27: TMemo;
+    Memo28: TMemo;
+    Memo29: TMemo;
+    Memo30: TMemo;
+    Memo31: TMemo;
+    Memo32: TMemo;
+    Memo33: TMemo;
+    Memo34: TMemo;
+    Memo35: TMemo;
+    Memo36: TMemo;
+    Memo37: TMemo;
+    Memo38: TMemo;
+    Memo39: TMemo;
+    Memo40: TMemo;
+    Memo41: TMemo;
+    Memo42: TMemo;
+    Panel2: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
